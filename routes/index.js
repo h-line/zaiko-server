e017b9de028d3aabf5e15377c059091d0dcde788
Lauri Hälinen <lauri@hiekkalaatikko.(none)>
2015-04-17 13:21:00 +0300

    githook

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var exec = require('child_process').exec;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Zaiko' });
+});
+
+
+router.post("/githook", function (req,res,next) {
+  exec("cd /home/lauri/Code/zaiko-server && git pull", {maxBuffer: 500*1024}, function(error, stdout, stderror){
+    return res.send("Out: "+stdout+"\nError: "+stderror);
+  });
 });
 
 module.exports = router;
