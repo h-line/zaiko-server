8f90302b06f83c21a3741a9c811688d85ab94178
Lauri Hälinen <lauri.halinen@gmail.com>
2015-04-17 13:51:22 +0300

    List model

---
+++ models/list.js
@@ -0,0 +1,30 @@
+var mongoose = require("mongoose");
+var model = mongoose.model;
+
+var listSchema = new mongoose.Schema({
+	client: String,
+	items: [{
+		name: String,
+		code: String,
+		amount: Number,
+		weight: Number,
+		packageSize: Number,
+		location: {
+			x: Number,
+			y: Number
+		}
+	}]
+});
+
+listSchema.statics = {
+	addList: function(client,callback) {
+		var listModel = this.model("List");
+		var list = new listModel();
+		list.client = client,
+		list.items = [];
+		list.save(callback);
+	},
+
+};
+
+module.exports = mongoose.model('List', schema);
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var exec = require('child_process').exec;
+var List = require("../models/list.js");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Zaiko' });
 });
 
+router.get("/lists/", function(req,res,next) {
+  List.find().exec(function(error, lists) {
+	return res.send(lists);
+  });
+});
 
 router.post("/githook", function (req,res,next) {
   exec("cd /home/lauri/Code/zaiko-server && git pull", {maxBuffer: 500*1024}, function(error, stdout, stderror){
